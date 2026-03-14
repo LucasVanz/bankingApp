64604449c8a83c5462757972ced97cd71d4f6bc7
Lucas Vanzella <lucasvanzellap2@gmail.com>
2026-03-14 17:03:10 -0300

    URL spring

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080' // A porta do Spring Boot
+  baseURL: import.meta.env.VITE_API_URL // A porta do Spring Boot
 });
 
 api.interceptors.request.use((config) => {
