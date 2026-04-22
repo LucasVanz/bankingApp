cd524140da4eea822e373f5c236da1213bf91319
Lucas Vanzella <lucasvanzellap2@gmail.com>
2026-04-21 23:54:41 -0300

    remove target

--- backend/banking-backend/target/classes/db/migration/V1__create_financial_asset_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE financial_assets (
-    id UUID PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    ticker VARCHAR(255) NOT NULL UNIQUE,
-    type VARCHAR(20) NOT NULL,
-    current_price NUMERIC(19,2) NOT NULL,
-    yield_percentage NUMERIC(10,4) NOT NULL,
-    last_update TIMESTAMP NOT NULL
-);
